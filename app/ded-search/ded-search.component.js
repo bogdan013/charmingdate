f22183b9353a2fea014381d6ae9ee08fd7fa16fc
Bogdan <my.sweetheart.agency@gmail.com>
2016-06-10 18:40:03 +0200

    ded profile view added

--- app/ded-search/ded-search.component.js
+++ app/ded-search/ded-search.component.js
@@ -1,19 +1,20 @@
+var dedSearch = angular.module('dedSearch');
 
-
-angular.module('dedSearch').component('dedSearch', {
+dedSearch.component('dedSearch', {
     templateUrl: 'ded-search/ded-search.template.html',
-
-    controller: ['$http', function dedSearchController($http) {
+    controller: ['$http', function DedSearchController($http) {
         var self = this;
-        self.searchId = '';
-        self.jsonList = [];
-
+            self.searchId = '';
+            self.jsonList = [];
 
         $http.get('serv/data.php').then(function(response){
             self.jsonList = response.data;
         });
+        
     }]
-}).filter('searchId', function(){
+});
+
+dedSearch.filter('searchId', function(){
     return function (v, query) {
         return v.filter(function (i) {
             return i.manId.indexOf(query) > -1;
