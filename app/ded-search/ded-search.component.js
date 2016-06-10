a9b7a30d2b4f275820da9346627a8e3e85983753
Bogdan <my.sweetheart.agency@gmail.com>
2016-06-10 16:16:28 +0200

    working filter search

--- app/ded-search/ded-search.component.js
+++ app/ded-search/ded-search.component.js
@@ -6,9 +6,21 @@ angular.module('dedSearch').component('dedSearch', {
     controller: ['$http', function dedSearchController($http) {
         var self = this;
 
+        self.searchId = 'CM';
+
+
         $http.get('serv/data.php').then(function(response){
             self.jsonList = response.data;
 
         });
     }]
+}).filter('searchId', function(){
+
+
+
+    return function (v, query) {
+        return v.filter(function (i) {
+            return i.manId.indexOf(query) > -1;
+        });
+    };
 });
