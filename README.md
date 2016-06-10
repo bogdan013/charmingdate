692ce666a01841195d445c93a7df84bc60ffb123
Bogdan <my.sweetheart.agency@gmail.com>
2016-06-10 14:16:40 +0200

    working with SERV directory

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Now its an empty readme file.
+Remember to create a config file named 'config.php' in /app/serv/. A file should contain
+
+const host_address = ''; // fill all the vars
+const login_database = '';
+const pass_database = '';
+const database_name = '';
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
 describe('my app', function() {
 
@@ -27,17 +26,4 @@ describe('my app', function() {
   });
 
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
